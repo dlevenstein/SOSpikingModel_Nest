da54f53ebdfe4c89d4fb115b24c0690c02eb7630
Jonathan Gornet <jmg1030@nyu.edu>
2016-12-05 15:30:44 -0500

    Computer_Neuroscience

--- volt_adap.py
+++ volt_adap.py
@@ -2,85 +2,83 @@ import nest
 import numpy
 import pylab
 
-for b in range(0, 100, 5):
+b = 70.0
 
-    b = float(b)
+nest.SetKernelStatus({"local_num_threads":8})
 
-    nest.SetKernelStatus({"local_num_threads":8})
+Current_increase = []
+Current_decrease = []
 
-    Current_increase = []
-    Current_decrease = []
+time_increase = ""
+time_decrease = ""
 
-    time_increase = ""
-    time_decrease = ""
+dict_params = {"V_peak" : 0.0
+, "V_reset" : -70.0
+, "t_ref" : 2.0
+, "g_L" :  30.0
+, "C_m" : 281.0
+, "E_ex" : 0.0
+, "E_in" : -85.0
+, "E_L" : -70.0
+, "Delta_T" : 2.0
+, "tau_w" : 100.0
+, "a" : 0.0
+, "b" : b
+, "V_th" : -55.0
+, "tau_syn_ex" : 0.2
+, "tau_syn_in" : 2.0
+, "I_e" : 200.0
+, "w": 0.0}
 
-    dict_params = {"V_peak" : 0.0
-    , "V_reset" : -70.0
-    , "t_ref" : 2.0
-    , "g_L" :  30.0
-    , "C_m" : 281.0
-    , "E_ex" : 0.0
-    , "E_in" : -85.0
-    , "E_L" : -70.0
-    , "Delta_T" : 2.0
-    , "tau_w" : 100.0
-    , "a" : 0.0
-    , "b" : b
-    , "V_th" : -55.0
-    , "tau_syn_ex" : 0.2
-    , "tau_syn_in" : 2.0
-    , "I_e" : 200.0
-    , "w": 0.0}
+neurons = nest.Create("aeif_cond_exp", 100)
+noise = nest.Create("poisson_generator")
 
-    neurons = nest.Create("aeif_cond_exp", 100)
-    noise = nest.Create("poisson_generator")
+nest.SetStatus(neurons, params=dict_params)
 
-    nest.SetStatus(neurons, params=dict_params)
+nest.SetStatus(noise, {"rate": 100.0})
 
-    nest.SetStatus(noise, {"rate": 100.0})
+for neuron in neurons:
+    nest.SetStatus([neuron], {"V_m": dict_params["E_L"]+(dict_params["V_th"]-dict_params["E_L"])*numpy.random.rand()})
 
-    for neuron in neurons:
-        nest.SetStatus([neuron], {"V_m": dict_params["E_L"]+(dict_params["V_th"]-dict_params["E_L"])*numpy.random.rand()})
+voltmeter = nest.Create("voltmeter")
 
-    voltmeter = nest.Create("voltmeter")
+spike_detector = nest.Create("spike_detector")
 
-    spike_detector = nest.Create("spike_detector")
+for n in neurons:
+    nest.Connect([n], spike_detector)
 
-    for n in neurons:
-        nest.Connect([n], spike_detector)
+nest.Connect(voltmeter, [neurons[0]])
+nest.Connect(noise, neurons)
 
-    nest.Connect(voltmeter, [neurons[0]])
-    nest.Connect(noise, neurons)
+K = 10
+d = 1.0
+J = 70.0
 
-    K = 10
-    d = 1.0
-    J = 70.0
+conn_dict = {"rule": "fixed_indegree", "indegree": K}
+syn_dict = {"delay": d, "weight": J}
 
-    conn_dict = {"rule": "fixed_indegree", "indegree": K}
-    syn_dict = {"delay": d, "weight": J}
+nest.Connect(neurons, neurons, conn_dict, syn_dict)
 
-    nest.Connect(neurons, neurons, conn_dict, syn_dict)
+for neuron in neurons:
+    nest.SetStatus([neuron], {"V_m": dict_params["E_L"]+(dict_params["V_th"]-dict_params["E_L"])*numpy.random.rand()})
 
-    for neuron in neurons:
-        nest.SetStatus([neuron], {"V_m": dict_params["E_L"]+(dict_params["V_th"]-dict_params["E_L"])*numpy.random.rand()})
+nest.SetStatus(neurons, params={"I_e": 400.0})
 
-    nest.SetStatus(neurons, params={"I_e": 400.0})
+nest.Simulate(1000.0)
 
-    nest.Simulate(1000.0)
+#nest.ResumeSimulation()
 
-    #nest.ResumeSimulation()
+#nest.SetStatus(neurons, params={"I_e": 0.0})
 
-    #nest.SetStatus(neurons, params={"I_e": 0.0})
+#nest.Simulate(1000.0)
 
-    #nest.Simulate(1000.0)
+dSD = nest.GetStatus(spike_detector, keys='events')[0]
+evs = dSD["senders"]
+ts_s = dSD["times"]
 
-    dSD = nest.GetStatus(spike_detector, keys='events')[0]
-    evs = dSD["senders"]
-    ts_s = dSD["times"]
-
-    dmm = nest.GetStatus(voltmeter)[0]
-    Vms = dmm["events"]["V_m"]
-    ts_v = dmm["events"]["times"]
+dmm = nest.GetStatus(voltmeter)[0]
+Vms = dmm["events"]["V_m"]
+ts_v = dmm["events"]["times"]
 
 
 
