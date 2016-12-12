14c7ca7247913939016a8fde83f1d399c96e9be6
jmg1030 <jmg1030@nyu.edu>
2016-12-12 18:12:51 -0500

    Computer_Neuroscience

--- statistical_analysis.py
+++ statistical_analysis.py
@@ -4,36 +4,68 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.mlab as mlab
 import math
+import scipy.stats as ss
 
-plt.figure("Spike Rate Analysis")
-plt.title("Spike Rate Analysis Normal Curve")
 
 for I in range(390,470, 10):
 
     open_file = open("spike_analysis_" + str(I) + ".json", "r")
     final_list = simplejson.load(open_file)
 
-    UP_mean = np.mean(final_list[0])
+    #print(final_list)
+
+    UP_state = final_list[0]
+    DOWN_state = final_list[1]
+
+    #UP_state = UP_state.sort()
+    #DOWN_state = DOWN_state.sort()
+
+
+    plt.figure("UP State External Current " + str(I))
+    plt.title("UP State Analysis")
+
+    """UP_mean = np.mean(final_list[0])
     UP_sd = np.std(final_list[0])
+    mu = UP_mean * 1000
+    variance = UP_sd * 1000
+    sigma = math.sqrt(variance)"""
+
+    """x = np.linspace(0, 250, 100)
+    plt.plot(x,mlab.normpdf(x, mu, sigma), label='UP ' + str(I))
+    plt.legend()"""
+
+    n, bins, patches = plt.hist(UP_state, 50, lw=3, fc=(0, 0, 0, 0.5))
+    plt.xlim(0,25)
+    plt.xlabel("Time, ms")
+    plt.ylabel("Number of UP states that last for x ms")
+
+    plt.savefig("UP State External Current " + str(I))
+
+
+
+    """UP_mean = np.mean(final_list[0])
+    UP_sd = np.std(final_list[0])
+
 
     DOWN_mean = np.mean(final_list[1])
     DOWN_sd = np.std(final_list[1])
 
-    print(UP_mean, UP_sd, DOWN_mean, DOWN_sd)
+
+
 
     mu = UP_mean
     variance = UP_sd
     sigma = math.sqrt(variance)
-    x = np.linspace(-10, 200, 100)
-    plt.plot(x,mlab.normpdf(x, mu, sigma), label='UP ' + str(I))
+    #x = np.linspace(ss.gamma.ppf(0.01, UP_state), ss.gamma.ppf(0.99, UP_state), 100)
+    #plt.plot(x,ss.gamma(x,mu,sigma), label='UP ' + str(400))
     #plt.legend()
+    #print(ss.gamma(x,mu,sigma))
 
     mu = DOWN_mean
     variance = DOWN_sd
     sigma = math.sqrt(variance)
     x = np.linspace(-10, 200, 100)
-    plt.plot(x,mlab.normpdf(x, mu, sigma), label='DOWN ' + str(I))
-    #plt.legend()
-
+    #plt.plot(x,mlab.normpdf(x, mu, sigma), label='DOWN ' + str(I))
+    #plt.legend()"""
 
 plt.show()
